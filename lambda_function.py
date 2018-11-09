c884a1db41f34c72d6f9106c672453b2fed96e49
DerekWendel <derek.r.wendel@gmail.com>
2018-11-09 10:29:43 -0500

    Field rename and put ticket to db
    Renamed "title" to "id"

--- lambda_function.py
+++ lambda_function.py
@@ -10,28 +10,23 @@ print('Loading function')
 def lambda_handler(event, context):
     print("live")
     sns = boto3.client(service_name="sns")
-    #print(json.dumps(event))
-    #print(json.dumps(context))
     
     # process attrs to check validity and create ticket
     json_d = json.loads(event)
     req_fields = ["title", "summary", "category", "event"]
+    
     if set(req_fields).issubset(set(json_d.keys())) and "user" in json_d["event"]:
-        ticket = {"title":json_d["category"] + ":" + json_d["event"]["user"], \
+        # format ticket from event
+        ticket = {"id":json_d["category"] + ":" + json_d["event"]["user"], \
                   "description":json["title"] + ":" + json_d["summary"], \
                   "body":json_d["event"]}
+        
         # Format put request for ticketdb
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         table = dynamodb.Table('Tickets')
         #converted_json = dbjson.dumps(json.dumps(event))
         table.put_item(
-            Item={
-                #converted_json
-                "title": dbjson.dumps(event["title"]),
-                "summary": dbjson.dumps(event["summary"]),
-                "category": dbjson.dumps(event["category"]),
-                "event": dbjson.dumps(event["event"])
-            }    
+            Item= ticket
         )
         #output = "{ \"Tickets\": [ { \"PutRequest\": { \"Item\": "
         #output += dbjson.dumps(json_d["event"])
@@ -42,7 +37,6 @@ def lambda_handler(event, context):
         # Then execute batch write
         # aws dynamodb batch-write-item --request-items file://data.json
        
-        
         return ticket
     else:
         return {"invalid":"malformed event error"}
