b8e60778e469ba290e122b5569c912fbd2bfcb84
DerekWendel <derek.r.wendel@gmail.com>
2018-11-29 20:54:10 -0500

    changed to expect sns events

--- lambda_function.py
+++ lambda_function.py
@@ -10,31 +10,32 @@ print('Loading function')
 def lambda_handler(event, context):
     print(event)
     sns = boto3.client(service_name="sns")
+    message = event["Records"][0]["Message"]
 
     # process attrs to check validity and create ticket
     req_fields = ["title", "summary", "category", "event"]
-    if set(req_fields).issubset(set(event.keys())) and "user" in event["event"].keys():
+    if set(req_fields).issubset(set(message.keys())) and "user" in message["event"].keys():
         # Connect to ticketdb
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         table = dynamodb.Table('Tickets')
         
         # Load event body json into dict
-        load_event = event["event"]
+        load_event = message["event"]
         print("about to push item")
-        response = table.get_item(Key={"id": event["category"] + ":" + load_event["user"]})
+        response = table.get_item(Key={"id": message["category"] + ":" + load_event["user"]})
         print(response)
         
         if "Item" not in response.keys():
             table.put_item(
                 Item={
                     # Format json for ticketdb
-                    "id": event["category"] + ":" + load_event["user"],
-                    "description": event["title"] + ":" + event["summary"],
-                    "body": event["event"]
+                    "id": message["category"] + ":" + load_event["user"],
+                    "description": message["title"] + ":" + message["summary"],
+                    "body": message["event"]
                 }
             )
             print("pushed, now exiting")
-            return "Ticket created: " + event["category"] + ":" + load_event["user"]
+            return "Ticket created: " + message["category"] + ":" + load_event["user"]
         print("Duplicate ticket")
         return "Duplicate ticket not created"
     else:
