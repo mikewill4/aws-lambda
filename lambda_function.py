ebc26408d7304262c1f2a10fafa85132288c58da
DerekWendel <derek.r.wendel@gmail.com>
2018-11-29 21:54:24 -0500

    style changes
    removed extra logging and cleaned things up

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,8 @@
 from __future__ import print_function
 from dynamodb_json import json_util as dbjson 
-
-import json
 import boto3
 
 print('Loading function')
-
 
 def lambda_handler(event, context):
     print(event)
@@ -15,20 +12,18 @@ def lambda_handler(event, context):
     
     # process attrs to check validity and create ticket
     req_fields = ["title", "summary", "category", "event"]
-    print(message["event"])
     # Load event body json into dict
     load_event = dbjson.loads(message["event"])
-    print(load_event)
+    
     if set(req_fields).issubset(set(message.keys())) and "user" in load_event.keys():
         # Connect to ticketdb
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         table = dynamodb.Table('Tickets')
         
-        print("about to push item")
         response = table.get_item(Key={"id": message["category"] + ":" + load_event["user"]})
-        print(response)
         
         if "Item" not in response.keys():
+            print("about to push item")
             table.put_item(
                 Item={
                     # Format json for ticketdb
